efd55c2ae0d7bc8100a9f51b8d8eb4680a8d979d
pexmee <pexmee@gmail.com>
2023-03-03 09:19:40 +0900

    stuff

--- lpn_sample_generator/lpn_sample_generator.py
+++ lpn_sample_generator/lpn_sample_generator.py
@@ -5,7 +5,8 @@ import numpy as np
 from numpy.typing import NDArray
 from sklearn.ensemble import ExtraTreesClassifier, RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
-
+from threading import Thread
+import json
 
 def generate_sample(s: NDArray, p: float, size: int) -> Tuple[NDArray, NDArray]:
     dim = len(s)
@@ -109,7 +110,7 @@ def extra_trees_classifier(A, b, dim, s):
         return -1
         
 
-def generic_classifier(classifier, A,b,dim,s):
+def generic_classifier(classifier, A,b,dim,s,result):
     classifier.fit(A,b)
 
     # Make prediction
@@ -117,14 +118,17 @@ def generic_classifier(classifier, A,b,dim,s):
 
     # Check if it is an acceptable candidate secret
     hamming_weight = np.mod(A @ candidate_secret + b, 2).sum()
-    print(hamming_weight)
+    print(f"hamming_weight: {hamming_weight}, int_hamming_weight: {int(hamming_weight)}")
 
     # Hamming Weight
-    if hamming_weight < 14000:
-        return hamming_weight
+    result.append(int(hamming_weight))
+    # m = 4*dim(1/2 - p)^-2
+    # t = m*p +sqrt(dim*m)
+    # if hamming_weight <= t: # 
+        # result.append(hamming_weight)
     
-    else:
-        return (False,hamming_weight)
+    # else:
+        # result.append(hamming_weight)
     
         
 
@@ -134,14 +138,37 @@ def generate_secrets(n,dim):
 
 def get_results(secrets, p, num_samples, dim):
     results = {"decision_class": [], "random_class" : [], "extra_class": []}
-
+    
+    threads = []
+    
     for i in range(len(secrets)):
         print(f"Generating result number #{i+1}")
+        decision_class = []
+        random_class = []
+        extra_class = []
+
         A,b = generate_sample(secrets[i],p,num_samples)
-        results["decision_class"].append(generic_classifier(DecisionTreeClassifier(),A,b,dim,secrets[i]))
-        results["random_class"].append(generic_classifier(RandomForestClassifier(),A,b,dim,secrets[i]))
-        results["extra_class"].append(generic_classifier(ExtraTreesClassifier(),A,b,dim,secrets[i]))
+        for classifier,lst in zip([DecisionTreeClassifier,RandomForestClassifier,ExtraTreesClassifier],[decision_class,random_class,extra_class]):
+            th = Thread(target=generic_classifier, args=(classifier(),A,b,dim,secrets[i],lst))
+            th.start()
+            threads.append(th)
+        
+        for th in threads:
+            th.join()
+
+        results["decision_class"]+=decision_class
+        results["random_class"]+=random_class
+        results["extra_class"]+=extra_class
+        # dt = Thread(target=generic_classifier,args=(DecisionTreeClassifier(),A,b,dim,secrets[i],decision_class))
+        # rt = Thread(target=generic_classifier,args=(RandomForestClassifier(),A,b,dim,secrets[i],random_class))
+        # et = Thread(target=generic_classifier,args=(ExtraTreesClassifier(),A,b,dim,secrets[i],extra_class))
+
+        # results["decision_class"].append(generic_classifier(DecisionTreeClassifier(),A,b,dim,secrets[i],decision_class))
+        # results["random_class"].append(generic_classifier(RandomForestClassifier(),A,b,dim,secrets[i],random_class))
+        # results["extra_class"].append(generic_classifier(ExtraTreesClassifier(),A,b,dim,secrets[i],extra_class))
+
     return results
+
 
         
         
@@ -150,17 +177,16 @@ def get_results(secrets, p, num_samples, dim):
 #     #print(f"sample #{i}:\n A: {result[0]}\n b: {result[1]}")
 
 num_secrets = 100
-dim = 16  
+dim = 10 
 s = np.random.randint(0, 2, dim)
 p = 0.125  # epsylon ->
-num_samples = 1000000  # m = number of samples
-
+num_samples = 200000  # m = number of samples
 A, b = generate_sample(s, p, num_samples)
-#decision_tree_classifier(A, b, dim, s)
-#random_forest_classifier(A, b, dim, s)
-#extra_trees_classifier(A, b, dim, s)
+
 results = get_results(generate_secrets(num_secrets,dim),p,num_samples,dim)
 print(f"decision class: {results['decision_class']}")
 print(f"random class: {results['random_class']}")
 print(f"extra class: {results['extra_class']}")
 
+with open("100_000_dim10.json","a") as file_h:
+    json.dump(results,fp=file_h)
